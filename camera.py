a66587fe9017a212ae7762b363e74d639ba9ace9
elg2605 <elg2605@vt.edu>
2023-02-28 02:32:45 -0500

    formatting camera download output - not done, battery died
    Former-commit-id: d0fcbc28887b93349d1d10cc85220cadc9b107cb

--- camera.py
+++ camera.py
@@ -3,15 +3,16 @@ import subprocess
 import os
         
 #python - install --upgrade pip
-import serial #python -m pip install pyserial
+#import serial #python -m pip install pyserial
 import time
 import datetime
 import threading
 import datetime
+import sys
 
 class camera:
 	def __init__(self):
-		self.ccapi = {T
+		self.ccapi = {
 			"URL":	"http://192.168.1.2:8080/ccapi",
 			"IP":	"192.168.1.2",			#ccapi network ip number
 			"PORT":	"8080",					#ccapi network port number
@@ -20,32 +21,32 @@ class camera:
 		}
 		self.debuglevel = ["normal","verbose","debug"]	#list for debug levels in logging
 		self.outputpath = os.path.dirname(__file__)	#gets path to python file
-		self.mutex = Lock()
-	pass
+		self.mutex = threading.Lock()
+		pass
 
-	def take(self, opfile="canomate-api-scripts/takecommand.txt", logging=0, debug=False):
+	def take(self, opfile="takecommand.txt", logging=0, debug=False):
 		args = [
 			"--ipaddress", f"{self.ccapi['IP']}", 
 			"--port", f"{self.ccapi['PORT']}",
 			"--opfile", opfile,
-			"--outputdir", f"\"{self.outputpath}\\Images\"",
+			"--outputdir", f"{self.outputpath}\\Images",
 			"--logginglevel", f"{self.debuglevel[logging]}"
         ]
 		#defines threads for camera take and for sensor gathering
 		camerathread = threading.Thread(target=self.request, args=(args, debug))
-		sensorthread = threading.Thread(target=self.duino, args=())
+		#sensorthread = threading.Thread(target=self.duino, args=())
 
 		#This creates a folder with the datetime for separating files
-		foldername = datetime.datetime.now().strftime("%I-%M%p-%B-%d-%Y")
-		subprocess.Popen([f"mkdir {foldername}"], shell=True)
+		#foldername = datetime.datetime.now().strftime("%I-%M%p-%B-%d-%Y")
+		#subprocess.Popen(["mkdir", f"{foldername}"], shell=True)
   
 		# Starts threads
-		sensorthread.start()
-		sleep(2)				#Sleep for 2 to allow sensor to start before camera image takes
+		#sensorthread.start()
+		time.sleep(2)				#Sleep for 2 to allow sensor to start before camera image takes
 		camerathread.start()
   
 		# Joins threads
-		sensorthread.join()
+		#sensorthread.join()
 		camerathread.join()
 
 	def help(self, debug=False):
@@ -60,38 +61,52 @@ class camera:
 		process = [
       		"python3", "Canomate/Canomate.py"
 		]
-  
 		process = process + args
 		if debug: print(process)
 		sp = subprocess.Popen(process, shell=True, stdout=subprocess.PIPE)
 		while sp.poll() is None:
 			line = sp.stdout.readline().decode()
-			print(line, end='')
-       
-       
-    #modify this to run constantly polling and storing arduino data, but use 
-    def duino(self, file='output.txt', iter=50, debug=False):
-        # port is a device name: depending on operating system. e.g. /dev/ttyUSB0 on GNU/Linux or COM3 on Windows.
-        # Add compatibility for linux and windows
-		ser = serial.Serial('COM3', 9800, timeout=1)
-		time.sleep(2)
-		pyfile = open(file, 'a')
 
-		for i in range(iter):
-			line = ser.readline()
-			if line:
-				string = line.decode()
-				timestamp = str(time.time())
-				num = (string)
-				if debug:
-					print("val", num, end='')
-					print("\ttime:", timestamp)
-				pyfile.write(num + ' ' + timestamp + '\n')
-		ser.close()
+			if (debug): print(line, end='')
+			if "DownloadNewFilesPolled" in line:
+				#Sample output ['GET', '/ccapi/ver100/contents/sd/117_PANA/IMG_3535.CR3', 'Downloading', '/ccapi/ver100/contents/sd/117_PANA/IMG_3535.CR3...', 'DownloadNewFilesPolled:', 'IMG_3535.CR3', 'stored', 'at', 'G:\\My', 'Drive\\School(VT)\\Engineering\\4805-4806', 'Senior', 'Design\\astronav\\Images\\IMG_3535.CR3', '[download', 'time', '=', '11.34', '(s)]']
+				splitline = line.split()
+				print("~~~")
+				print(splitline)
+				print("~~~")
+				if len(splitline) > 5:
+					imgname = splitline[5]
+					imgpath = "\"" + " ".join(splitline[8:11]) + "\""
+					print(f"Filename: {imgname}\t{imgpath}")
+       
+       
+    # 	#modify this to run constantly polling and storing arduino data, but use 
+    # def duino(self, file='output.txt', iter=50, debug=False):
+	# 	# port is a device name: depending on operating system. e.g. /dev/ttyUSB0 on GNU/Linux or COM3 on Windows.
+	# 	# Add compatibility for linux and windows
+	# 	ser = serial.Serial('COM3', 9800, timeout=1)
+	# 	time.sleep(2)
+	# 	pyfile = open(file, 'a')
+
+	# 	for i in range(iter):
+	# 		line = ser.readline()
+	# 		if line:
+	# 			string = line.decode()
+	# 			timestamp = str(time.time())
+	# 			num = (string)
+	# 			if debug:
+	# 				print("val", num, end='')
+	# 				print("\ttime:", timestamp)
+	# 			pyfile.write(num + ' ' + timestamp + '\n')
+	# 	ser.close()
 
 
 if __name__ == '__main__':
+	debug = True#False
 	cam = camera()
-	cam.take(debug=True)
+	if "down" in str(sys.argv):
+		cam.take(opfile="download.txt", debug=debug)
+	else:
+		cam.take(debug=debug)
 	pass
 
