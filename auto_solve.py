76ae9faadd881885c732f0597d4b660f6aa9e075
sayf-e <60414689+sayf-e@users.noreply.github.com>
2023-03-02 14:05:11 -0500

    write all results to single csv

--- auto_solve.py
+++ auto_solve.py
@@ -7,6 +7,8 @@ import re
 import pandas
 import csv
 from pathlib import Path
+import pandas as pd
+import glob
 
 
 def plate_solve(filename, database = 'v17'):
@@ -30,17 +32,21 @@ def parse_sol(filename):
     :return: ra_val: right ascension value of center of image in decimal degrees (as a float)
     :return: dec_val: declination value of center of image in decimal degrees
     """
-    with open(os.path.dirname(filename) + '/' + Path(filename).stem + '.wcs') as file:
-        for line in file:
-            if line[0:2] == 'JD':
-                jd_date = re.findall(r'\d+\.?\d*', line)
-                gmt_date = pandas.to_datetime(float(jd_date[0]), origin='julian', unit='D')
-            elif line[0:6] == 'CRVAL1':
-                ra_val = float(re.findall('-?\d*\.?\d+E[+-]?\d+', line)[0])
-            elif line[0:6] == 'CRVAL2':
-                dec_val = float(re.findall('-?\d*\.?\d+E[+-]?\d+', line)[0])
+    if os.path.isfile(os.path.dirname(filename) + '/' + Path(filename).stem + '.wcs'):
+        with open(os.path.dirname(filename) + '/' + Path(filename).stem + '.wcs') as file:
+            for line in file:
+                if line[0:2] == 'JD':
+                    jd_date = re.findall(r'\d+\.?\d*', line)
+                    gmt_date = pandas.to_datetime(float(jd_date[0]), origin='julian', unit='D')
+                elif line[0:6] == 'CRVAL1':
+                    ra_val = float(re.findall('-?\d*\.?\d+E[+-]?\d+', line)[0])
+                elif line[0:6] == 'CRVAL2':
+                    dec_val = float(re.findall('-?\d*\.?\d+E[+-]?\d+', line)[0])
 
-    return [gmt_date, ra_val, dec_val]
+        return [gmt_date, ra_val, dec_val]
+    else:
+        return ['F', 'F', 'F']
+
 
 def write_single_sol(filename, date_ra_dec):
     """
@@ -49,9 +55,50 @@ def write_single_sol(filename, date_ra_dec):
     :param date_ra_dec: array of date, ra, dec
     :return: none
     """
-    with open(os.path.dirname(filename) + '/' + Path(filename).stem + '.csv', 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow([Path(filename).stem, date_ra_dec[0], date_ra_dec[1], date_ra_dec[2]])
+    if os.path.isfile(os.path.dirname(filename) + '/' + Path(filename).stem + '.wcs'):
+        with open(os.path.dirname(filename) + '/' + os.path.basename(os.path.dirname(filename)) + '.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['Image', 'Dec', 'RA ', 'Date', 'Time'])
+            writer.writerow([Path(filename).stem,  date_ra_dec[1], date_ra_dec[2], date_ra_dec[0].date(), date_ra_dec[0].time()])
+    else:
+        with open(os.path.dirname(filename) + '/' + os.path.basename(os.path.dirname(filename)) + '.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['Image', 'Dec', 'RA ', 'Date', 'Time'])
+            writer.writerow([Path(filename).stem, 'f', 'f', 'f', 'f'])
+
+
+
+def evaluate_run(test_directory):
+    """
+    evaluate_run: after solving all the caputres in a test, write a CSV file with all the results
+    :param test_directory: test directory is the "tests/???" directory containing something like
+        test_0/
+            captures/
+                capture_0/
+                capture_1/
+                ...
+            ...
+    eg. evaluate_run(c:/test0) should have test0/captures/captures0
+    :return: none
+    """
+
+    captures_dir = f"{test_directory}/captures/"
+    test_name = os.path.basename(test_directory)
+    all_dirs = next(os.walk(captures_dir))[1]
+    print(test_name)
+
+    all_files = []
+    for dirs in all_dirs:
+        all_files.append(captures_dir + '/' + dirs + '/' + dirs + '.csv')
+    print(all_dirs)
+
+    li = pd.DataFrame()
+
+    for filename in all_files:
+        df = pd.read_csv(filename)
+        li = pd.concat([li, df], ignore_index=True)
+    # print(os.path.basename(str(Path(test_directory).parents[0])))
+    li.to_csv(test_directory + "/" + test_name + '.csv', index=False)
 
 
 def auto_solve(filename):
@@ -65,7 +112,31 @@ def auto_solve(filename):
     write_single_sol(filename, solution)
 
 
+
+
 directory_0 = 'tests/test_0/captures/capture_0/'
 filename_0 = 'IMG_3504.CR3'
 
-auto_solve(directory_0 + filename_0)
+directory_1 = 'tests/test_0/captures/capture_1/'
+filename_1 = 'IMG_3505.CR3'
+
+directory_2 = 'tests/test_0/captures/capture_2/'
+filename_2 = 'IMG_3506.CR3'
+
+directory_3 = 'tests/test_0/captures/capture_3/'
+filename_3 = 'IMG_3509.CR3'
+
+directory_4 = 'tests/test_0/captures/capture_4/'
+filename_4 = 'IMG_3510.CR3'
+
+directory_5 = 'tests/test_0/captures/capture_5/'
+filename_5 = 'IMG_3511.CR3'
+
+#auto_solve(directory_0 + filename_0)
+#auto_solve(directory_1 + filename_1)
+#auto_solve(directory_2 + filename_2)
+#auto_solve(directory_3 + filename_3)
+#auto_solve(directory_4 + filename_4)
+#auto_solve(directory_5 + filename_5)
+
+#evaluate_run('tests/test_0')
